eb4739192fbcbc075f8d244dbb96d99a8dba3770
Tami Fedrigo <tamifedrigo@Tamis-MacBook-Pro.local>
2021-04-28 18:38:26 -0400

    first commit

--- src/script.js
+++ src/script.js
@@ -1,3 +1,57 @@
 "use strict";
 
 // TODO - write your code here.
+
+const randomDamage = () => {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+//NEED TO FIX THIS, NOT CORRECT
+const chooseOption = (opt1, opt2) => {
+    let randNum = Math.floor(Math.random() * 2);
+    //should it be this instead?
+    //const ranNum = Math.round(Math.random());
+     return randNum === 0 ? opt1 : opt2;
+}
+
+const attackPlayer = function(health) {
+    return health - randomDamage;
+};
+
+const logHealth = (player, health) => {
+    console.log(`${player} health: ${health}`);
+}
+
+const logDeath = (winner, loser) => {
+    console.log(`${winner} defeated ${loser}`);
+}
+
+const isDead = (health) => {
+    if (health <= 0); {
+    return true;
+} else {
+    return false;
+}
+}
+
+const fight = function(player1, player2, player1Health, player2Health) {
+        while(true) {
+            const attacker = chooseOption(player1, player2);
+            if (attacker === player1) {
+                player2Health = attackPlayer(player2Health);
+                logHealth(player2, player2Health);
+                if (isDead(player2Health)) {
+                    logDeath(player1, player2);
+                }
+    
+            } else {
+                player1Health = attackPlayer(player1Health);
+                logHealth(player1, player1Health);
+                if (isDead(player1Health)) {
+                    logDeath(player2, player1);
+                    break;
+    
+                }
+            }
+        }
+    }
